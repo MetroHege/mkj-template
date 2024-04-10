b3d7e8bd478258b51929a3465b832f7fd7f26612
MetroHege <113589548+MetroHege@users.noreply.github.com>
2024-04-10 13:01:45 +0300

    test

--- wp-content/themes/example-theme/category.php
+++ wp-content/themes/example-theme/category.php
@@ -12,6 +12,7 @@ get_header();
         </div>
         <img src="<?php echo get_random_post_image(get_queried_object_id()); ?>"/>
     </section>
+<p>test</p>
     <main>
         <section class="products">
             <h2><?php single_cat_title(); ?></h2>
